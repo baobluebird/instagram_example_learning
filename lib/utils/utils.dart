9f798cf362f8328e5c7ad3e136f61b68ffa16106
blubird <lebaohv1236bqt@gmail.com>
2023-10-14 08:33:28 +0700

    first commit

---
+++ lib/utils/utils.dart
@@ -0,0 +1,19 @@
+import 'package:image_picker/image_picker.dart';
+import 'package:flutter/material.dart';
+
+pickImage(ImageSource source) async {
+  final ImagePicker imagePicker = ImagePicker();
+  XFile? file = await imagePicker.pickImage(source: source);
+  if (file != null) {
+    return await file.readAsBytes();
+  }
+}
+
+// for displaying snackbars
+showSnackBar(BuildContext context, String text) {
+  return ScaffoldMessenger.of(context).showSnackBar(
+    SnackBar(
+      content: Text(text),
+    ),
+  );
+}
